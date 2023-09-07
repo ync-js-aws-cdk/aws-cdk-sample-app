873ae85bb221fa2eaf4bbfd51a97bbe9d3169092
yongnan <ync@fastmail.com>
2023-09-07 14:36:19 +0800

    u

--- bin/sample-app.ts
+++ bin/sample-app.ts
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { SampleAppStack } from '../lib/sample-app-stack';
+//import { SampleAppStack } from '../lib/sample-app-stack';
+import { S3BucketStack } from '../lib/S3BucketStack';
 
 const app = new cdk.App();
-new SampleAppStack(app, 'SampleAppStack');
+//new SampleAppStack(app, 'SampleAppStack');
+new S3BucketStack(app, 'S3BucketStack', {
+  env: {
+    region: 'ap-southeast-2',
+    account: '049291762274',
+  },  
+});
+app.synth();
